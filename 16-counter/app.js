b6b9e8f54de7006545bfc0080e5241a678469987
Freemasoid <freemasoid@gmail.com>
2023-10-02 22:13:39 +0200

    commit

---
+++ 16-counter/app.js
@@ -0,0 +1,38 @@
+function getElement(selection) {
+  const element = document.querySelector(selection);
+  if (element) {
+    return element;
+  }
+  throw new Error(`Please check "${selection}" selector, no such element exist`);
+}
+
+function Counter(element, value) {
+  this.counter = element;
+  this.value = value;
+  this.resetBtn = element.querySelector(".reset");
+  this.increaseBtn = element.querySelector(".increase");
+  this.decreaseBtn = element.querySelector(".decrease");
+  this.valueDOM = element.querySelector(".value");
+  this.valueDOM.textContent = this.value;
+}
+
+Counter.prototype.increase = function () {
+  console.log(this);
+  this.value++;
+  this.valueDOM.textContent = this.value;
+};
+
+Counter.prototype.decrease = function () {
+  console.log(this);
+  this.value--;
+  this.valueDOM.textContent = this.value;
+};
+
+Counter.prototype.reset = function () {
+  console.log(this);
+  this.value = 0;
+  this.valueDOM.textContent = this.value;
+};
+
+const firstCounter = new Counter(getElement(".first-counter"), 100);
+const secondCounter = new Counter(getElement(".second-counter"), 200);
